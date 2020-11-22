035c9844aaf7d300fba437469a269e9b1f7b6bbd
Gino <ginocote@gmail.com>
2020-11-21 19:31:42 -0500

    New rss to json
    May be this one is less buggy. https://github.com/nasa8x/rss-to-json/blob/master/src/rss.js

--- rss.js
+++ rss.js
@@ -1,147 +1,175 @@
-
 // Invoke 'strict' JavaScript mode
-'use strict';
- var  util = require('util'),
-      // xml2js = require('xml2js'), // try this one if it's better
-      xml2js = require('xml2json'),
-      request = require('request');
-
-
-function copyEntry(obj){
-   var result = {};
-   for (var x in obj) {
-       result[x] = obj[x][0];
-   }
-   return result;
-}
+//'use strict';
+var util = require('util'),
+  xml = require('xml2json'),
+  axios = require('axios');
 
 
 module.exports = {
-    load: function(url, callback){
-      var $ = this;
-      request({
-            url: url,
-            headers: {
-              //'User-Agent': 'RSS Reader',
-              'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:45.0) Gecko/20100101 Firefox/45.0',
-              'accept': 'text/html,application/xhtml+xml'
-              //'accept': '*/*'
-            },
-            pool: false,
-            followRedirect: true
+  isUrl: function (s) {
+    return s && /((http(s)?):\/\/[\w\.\/\-=?#]+)/i.test(s);
+  },
+  load: function (x, callback) {
 
-          }, function (error, response, xml) {
-                if (!error && response.statusCode == 200) {
-                  var parser = new xml2js.Parser({trim: false, normalize: true, mergeAttrs: true});
-                  parser.addListener("error", function(err) {
-                      callback(err, null);
-                  });
-                  parser.parseString(xml, function (err, result) {
-                    if(err){
-                      callback(err, null);
-                      return;
-                    }
-                    else{
-                      var rss = $.parser(result);
-                      callback(null, rss);
-                    }
-                  });
-                }
-                else{
-                  console.log("Error", error);
-                  callback(new Error(
-                      'Error loading feed' + 
-                      ((response && response.statusCode)?  (' : ' + response.statusCode) : '')
-                  ));
-                  this.emit('error', new Error('Bad status code'));
-                }
-              });
+    var _this = this;
+    var o = {
+      method: 'get',
+      headers: {
+        //'User-Agent': 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36 OPR/68.0.3618.63',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+      }
 
-    },
-    parser: function(json){
-        var channel = json.rss.channel;
-        var rss = {item:[]};
-        if(util.isArray(json.rss.channel))
-          channel = json.rss.channel[0];
+    };
 
-        if (channel.title) {
-            rss.title = channel.title[0];
-         }
-         if (channel.description) {
-             rss.description = channel.description[0];
-         }
-         if (channel.image) {
-             rss.image = channel.image[0];
-         }
-     
-//          if(util.isArray(channel.image)) // GC
-//           rss.image = channel.image[0];
-//          else
-//           rss.image = '';
-     
-         if (channel.link) {
-             rss.url = channel.link[0];
-         }
-         if (channel.item) {
-           if (!util.isArray(channel.item)) {
-             channel.item = [channel.item];
-           }
-           channel.item.forEach(function(val){
-             var obj = {};
-             obj.title = !util.isNullOrUndefined(val.title)?val.title[0]:'';
-             obj.description = !util.isNullOrUndefined(val.description)?val.description[0]:'';
-            
-             // GC Add content:encoded
-             // obj.author = !util.isNullOrUndefined(val.creator)?val.creator[0]:'';
-             if (val['dc:creator']) {
-               obj.author = val['dc:creator'];
-             }
-            
-             // GC Add content:encoded
-             if (val['content:encoded']) {
-               obj.content = val['content:encoded'];
-             }
-             obj.url = obj.link = !util.isNullOrUndefined(val.link)?val.link[0]:'';
-
-             if (val.pubDate) {
-               //obj.created = Date.parse(val.pubDate[0]);
-               obj.pubDate = val.pubDate[0];
-             }
-             if (val['media:content']) {
-               obj.media = val.media || {};
-               obj.media.content = val['media:content'];
-             }
-             if (val['media:thumbnail']) {
-                obj.media = val.media || {};
-                obj.media.thumbnail = val['media:thumbnail'];
-             }
-             if (val['itunes:duration']){
-                 obj.duration = val['itunes:duration'][0];
-             }
-             if(val.enclosure){
-                if(!util.isArray(val.enclosure)){
-                    obj.enclosure = copyEntry(val.enclosure);
-                }
-                else if(val.enclosure.length == 1){
-                    obj.enclosure = copyEntry(val.enclosure[0]);
-                }
-                else{
-                    obj.enclosure = [];
-                    val.enclosure.forEach(function(enclosure){
-                      obj.enclosure.push(copyEntry(enclosure));
-                    });
-                }
-              }
-              rss.item.push(obj);
-
-           });
-
-         }
-         return rss;
-
-    },
-    read: function(url, callback){
-      return this.load(url, callback);
+    if (typeof x === 'object' && x !== null) {
+      o = Object.assign(o, x);
+    } else if (this.isUrl(x)) {
+      o = Object.assign(o, { url: x });
     }
+
+    return new Promise(function (resolve, reject) {
+      axios(o).then(function ({ data }) {
+        var result = _this.parser(data);
+        callback && callback(null, result);
+        resolve(result);
+
+      }).catch(function (err) {
+        console.log(err);
+        callback && callback(err, null);
+        reject(err);
+      })
+    });
+
+  },
+  parser: function (data) {
+
+    var rss = { items: [] };
+    var result = xml.toJson(data, { object: true });
+
+    // console.log(result.rss.channel.item[0]);
+
+    // return;
+
+    var channel = result.rss && result.rss.channel ? result.rss.channel : result.feed;
+    if (util.isArray(channel)) channel = channel[0];
+
+    var items = channel.item || channel.entry;
+
+    if (channel.title) {
+      rss.title = channel.title;
+    }
+
+    if (channel.description) {
+      rss.description = channel.description;
+    }
+
+    rss.link = channel.link && channel.link.href ? channel.link.href : channel.link;
+    rss.category = channel.category || [];
+
+    if (channel.image) {
+      rss.image = channel.image.url;
+    }
+
+    if (!rss.image && channel["itunes:image"]) {
+      rss.image = channel['itunes:image'].href;
+    }
+
+    if(items && !Array.isArray(items)) {
+      // the main 'items' code block below expects the items variable to be an Array of Objects
+      // but if in the originating XML the channel element contains only a single item, the items
+      // variable is set to a literal Object instead of an Array of Objects of length 1. It is fixed
+      // here so the items variable behaves the same whether it contains 1 or many items
+      items = [items];
+    }
+
+    if (items && items.length > 0) {
+
+      for (let i = 0; i < items.length; i++) {
+        var val = items[i];
+
+        var obj = {};
+        obj.title = val.title && val.title.$t ? val.title.$t : val.title;
+        obj.id = val.guid && val.guid.$t ? val.guid.$t : val.id;
+        obj.description = val.summary && val.summary.$t ? val.summary.$t : val.description;
+        obj.url = val.link && val.link.href ? val.link.href : val.link;
+        obj.link = obj.url;
+        obj.author = val.author && val.author.name ? val.author.name : val['dc:creator'];
+        obj.published = val.created ? Date.parse(val.created) : val.pubDate ? Date.parse(val.pubDate) : Date.now();
+        obj.created = val.updated ? Date.parse(val.updated) : val.pubDate ? Date.parse(val.pubDate) : val.created ? Date.parse(val.created) : Date.now;
+        obj.category = val.category || [];
+        obj.content = val.content && val.content.$t ? val.content.$t : null;
+
+        // Medium Support via @sstrubberg
+        if (val["content:encoded"]) {
+          obj.content_encoded = val["content:encoded"];
+          obj.content = obj.content_encoded;
+        }
+
+        if (val['itunes:subtitle']) {
+          obj.itunes_subtitle = val['itunes:subtitle'];
+        }
+        if (val['itunes:summary']) {
+          obj.itunes_summary = val['itunes:summary'];
+        }
+        if (val['itunes:author']) {
+          obj.itunes_author = val['itunes:author'];
+        }
+        if (val['itunes:explicit']) {
+          obj.itunes_explicit = val['itunes:explicit'];
+        }
+        if (val['itunes:duration']) {
+          obj.itunes_duration = val['itunes:duration'];
+        }
+        if (val['itunes:season']) {
+          obj.itunes_season = val['itunes:season'];
+        }
+        if (val['itunes:episode']) {
+          obj.itunes_episode = val['itunes:episode'];
+        }
+        if (val['itunes:episodeType']) {
+          obj.itunes_episode_type = val['itunes:episodeType'];
+        }
+
+        obj.enclosures = val.enclosure ? util.isArray(val.enclosure) ? val.enclosure : [val.enclosure] : [];
+
+        if (val['media:thumbnail']) {
+          obj.media = val.media || {};
+          obj.media.thumbnail = val['media:thumbnail'];
+          obj.enclosures.push(val['media:thumbnail']);
+        }
+
+        if (val['media:content']) {
+          obj.media = val.media || {};
+          obj.media.content = val['media:content'];
+
+          obj.enclosures.push(val['media:content']);
+        }
+
+        if (val['media:group']) {
+
+          if (val['media:group']['media:title'])
+            obj.title = val['media:group']['media:title'];
+
+          if (val['media:group']['media:description'])
+            obj.description = val['media:group']['media:description'];
+
+          if (val['media:group']['media:thumbnail'])
+            obj.enclosures.push(val['media:group']['media:thumbnail'].url);
+
+
+        }
+
+        rss.items.push(obj);
+      }
+    }
+
+    return rss;
+
+  },
+
+  read: function (url, callback) {
+    return this.load(url, callback);
+  }
 
 };
